ed09c2446251bcd924e69c86437d5902bea11963
leprikon8888 <leprikon8888@gmail.com>
2023-10-20 20:59:55 +0300

    подключил страницы логин и регистрация (формы еще неактивны)

--- store/settings.py
+++ store/settings.py
@@ -36,7 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'users',
 ]
+
+
+
+# Установите кодировку на UTF-8
+DEFAULT_CHARSET = 'utf-8'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,3 +133,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Users
+
+AUTH_USER_MODEL = 'users.User'
