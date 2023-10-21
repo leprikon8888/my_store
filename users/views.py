3d9060fc23826f37b0ba44440592f33e87da93c7
leprikon8888 <leprikon8888@gmail.com>
2023-10-21 21:26:58 +0300

    подключил страницы логин и регистрация (формы активны и работают)

--- users/views.py
+++ users/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponsePermanentRedirect
+from django.contrib import auth
+from users.forms import UserLoginForm, UserRegistrationForm
+from django.urls import reverse
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponsePermanentRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponsePermanentRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
