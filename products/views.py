6adfa36d3547ada1643fc65e6829df8feba7fc61
leprikon8888 <leprikon8888@gmail.com>
2023-10-23 11:57:11 +0300

    добавил фильтрацию по категориям

--- products/views.py
+++ products/views.py
@@ -13,11 +13,17 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None):
+    # if category_id:
+    #     products = Product.objects.filter(category_id=category_id)
+    # else:
+    #     products = Product.objects.all()
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+
     context = {
         'title': 'Store - Каталог',
-        'products': Product.objects.all(),
         'categoties': ProductCategory.objects.all(),
+        'products': products,
     }
     return render(request, 'products/products.html', context)
 
