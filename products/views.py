040b441f312fc23a4923a676c2a59193867fcc5e
leprikon8888 <leprikon8888@gmail.com>
2023-10-23 21:33:58 +0300

    перевел вьюшку products из метода в класс Листвью, сохранивши функционал c пагинацией и тд

--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import  HttpResponseRedirect
 from products.models import Product, ProductCategory, Basket
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+
 
 # функции = контролеры = вьюхи
 
@@ -16,23 +17,21 @@ class IndexView(TemplateView):
         return context
 
 
-def products(request, category_id=None, page_number=1):
-    # if category_id:
-    #     products = Product.objects.filter(category_id=category_id)
-    # else:
-    #     products = Product.objects.all()
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    per_page =3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
 
+    def get_queryset(self):
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
 
-    context = {
-        'title': 'Store - Каталог',
-        'categoties': ProductCategory.objects.all(),
-        'products': products_paginator,
-    }
-    return render(request, 'products/products.html', context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductsListView, self).get_context_data()
+        context['title'] = 'Store - Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
 
 
 @login_required
