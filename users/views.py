8149726c3add0f5f16eb20afec6a96d5eb94337c
leprikon8888 <leprikon8888@gmail.com>
2023-10-24 15:41:36 +0300

    подчистил мусор в коде

--- users/views.py
+++ users/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth, messages
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.urls import reverse, reverse_lazy
 from products.models import Basket
-from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
 from users.models import User
@@ -40,45 +37,3 @@ class UserProfileView(UpdateView):
         context['baskets'] = Basket.objects.filter(user=self.object)
         return context
 
-
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
-
-
-
-
-# def registration(request):
-#     if request.method == 'POST':
-#         form = UserRegistrationForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Поздравляем, регистрация успешна!')
-#             return HttpResponseRedirect(reverse('users:login'))
-#     else:
-#         form = UserRegistrationForm()
-#     context = {'form': form}
-#     return render(request, 'users/registration.html', context)
-
-
-
-
-
-# @login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('users:profile'))
-#         else:
-#             print(form.errors)
-#     else:
-#         form = UserProfileForm(instance=request.user)
-#
-#     context = {
-#         'title': 'Store - Профиль',
-#         'form': form,
-#         'baskets': Basket.objects.filter(user=request.user)
-#     }
-#     return render(request, 'users/profile.html', context)
