34983ff7e78e404ec2f0b6048ebf479743d59474
leprikon8888 <leprikon8888@gmail.com>
2023-11-01 19:16:04 +0200

    написал элементарный тест

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from http import HTTPStatus
 
-# Create your tests here.
+class IndexViewTestCase(TestCase):
+
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store')
+        self.assertTemplateUsed(response, 'products/index.html')
