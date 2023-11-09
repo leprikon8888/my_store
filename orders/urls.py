6cdf6183d614229714401c73d148183e4839bcf6
leprikon8888 <leprikon8888@gmail.com>
2023-11-09 11:44:59 +0200

    Создание страницы заказов

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from orders.views import OrderCreateView, SuccessTemplateView, CanceledTemplateView
+from orders.views import OrderCreateView, SuccessTemplateView, CanceledTemplateView, OrderListView
 
 from products.views import ProductsListView, basket_add, basket_remove
 
@@ -8,6 +8,7 @@ app_name = 'orders'
 
 urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name='order-create'),
+    path('', OrderListView.as_view(), name='orders_list'),
     path('order-success/', SuccessTemplateView.as_view(), name='order_success'),
     path('order-canceled/', CanceledTemplateView.as_view(), name='order_canceled'),
 ]
